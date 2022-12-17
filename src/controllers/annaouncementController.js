d25edb8a0a48931723ba5776b5cd1c818a46a549
YoussefMohamedSalah <edge.mohamed120@gmail.com>
2022-12-17 05:44:30 +0200

    asyncHandler

--- src/controllers/annaouncementController.js
+++ src/controllers/annaouncementController.js
@@ -1,17 +1,19 @@
+const asyncHandler = require('express-async-handler')
+
 // ## desc  get Announcements
 // ## route GET /api/annaouncement
 // ## access Public
 
-const getAnnouncements = (req, res) => {
+const getAnnouncements = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get Annaouncement' })
-}
+})
 // -----*-----*-----*-----*-----*-----*-----*-----*-----*
 
 // ## desc  post Announcements
 // ## route POST /api/annaouncement
 // ## access Public
 
-const postAnnouncement = (req, res) => {
+const postAnnouncement = asyncHandler(async (req, res) => {
     const { teacher_name, subject, message } = req.body
     if (!message) {
         res.status(400)
@@ -19,35 +21,35 @@ const postAnnouncement = (req, res) => {
     }
 
     res.status(200).json({ message: 'Post Annaouncement' })
-}
+})
 // -----*-----*-----*-----*-----*-----*-----*-----*-----*
 
 // ## desc  update Announcements
 // ## route PUT /api/annaouncement/:id
 // ## access Public
 
-const updateAnnouncement = (req, res) => {
+const updateAnnouncement = asyncHandler(async (req, res) => {
     const id = req.params.id
     if (!id) {
         res.status(400)
         throw new Error('Can Not Found Announcement')
     }
     res.status(200).json({ message: `Update Annaouncement with id : ${id}` })
-}
+})
 // -----*-----*-----*-----*-----*-----*-----*-----*-----*
 
 // ## desc  delete Announcements
 // ## route DELETE /api/annaouncement/:id
 // ## access Public
 
-const deleteAnnouncement = (req, res) => {
+const deleteAnnouncement = asyncHandler(async (req, res) => {
     const id = req.params.id
     if (!id) {
         res.status(400)
         throw new Error('Can Not Found Announcement')
     }
     res.status(200).json({ message: `Delete Annaouncement with id : ${id}` })
-}
+})
 // -----*-----*-----*-----*-----*-----*-----*-----*-----*
 
 
--- src/controllers/dueTaskController.js
+++ src/controllers/dueTaskController.js
@@ -1,17 +1,19 @@
+const asyncHandler = require('express-async-handler')
+
 // ## desc  get Due Tasks
 // ## route GET /api/due_task
 // ## access Public
 
-const getDueTasks = (req, res) => {
+const getDueTasks = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get Due Tasks' })
-}
+})
 // -----*-----*-----*-----*-----*-----*-----*-----*-----*
 
 // ## desc  post Due Task
 // ## route POST /api/due_task
 // ## access Public
 
-const postDueTask = (req, res) => {
+const postDueTask = asyncHandler(async (req, res) => {
     const { title, course, topic, due_to } = req.body
     if (!course) {
         res.status(400)
@@ -19,35 +21,35 @@ const postDueTask = (req, res) => {
     }
 
     res.status(200).json({ message: 'Post Due Task' })
-}
+})
 // -----*-----*-----*-----*-----*-----*-----*-----*-----*
 
 // ## desc  update Due Task
 // ## route PUT /api/due_task/:id
 // ## access Public
 
-const updateDueTask = (req, res) => {
+const updateDueTask = asyncHandler(async (req, res) => {
     const id = req.params.id
     if (!id) {
         res.status(400)
         throw new Error('Can Not Found Task')
     }
     res.status(200).json({ message: `Update Due Task with id : ${id}` })
-}
+})
 // -----*-----*-----*-----*-----*-----*-----*-----*-----*
 
 // ## desc  delete Due Task
 // ## route DELETE /api/due_task/:id
 // ## access Public
 
-const deleteDueTask = (req, res) => {
+const deleteDueTask = asyncHandler(async (req, res) => {
     const id = req.params.id
     if (!id) {
         res.status(400)
         throw new Error('Can Not Found Task')
     }
     res.status(200).json({ message: `Delete Due Task with id : ${id}` })
-}
+})
 // -----*-----*-----*-----*-----*-----*-----*-----*-----*
 
 
